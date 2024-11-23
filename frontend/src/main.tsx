c85d60d9dc404ca7f1f0873777b1ef3e74f11f0d
Alison Fernando <alisonteobaldo2001@gmail.com>
2024-11-22 21:31:12 -0300

    Initial commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <h1>Hello from TypeScript Frontend with Vite!</h1>
-    </div>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
